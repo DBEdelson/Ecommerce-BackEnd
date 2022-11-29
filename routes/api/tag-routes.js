3de90662925d51bca6818e999158d5cc4cc3d9fe
DBEdelson <DBEdelson8@gmail.com>
2022-11-28 20:06:08 -0600

    intial

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,83 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  // find all tags
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id`
+  try {
+    const tagData = await Tag.findByPk (req.params.id , { 
+       // be sure to include its associated Product data
+      include: [{ model: Product }], 
+    });
+    if(!tagData) {
+      res.status(404).json({Message: 'No tag found with that id!'})
+      return
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err)
+  } 
+});
+
+router.post('/', async (req, res) => {
+  // create a new tag
+try {
+  const newTag = await Tag.create(req.body);
+  res.status(200).json(newTag);
+} catch (err) {
+  res.status(400).json(err);
+}
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+  Tag.update(
+    {
+      name: req.body.name,
+      id: req.body.id,
+    },
+    {
+      where: {
+        tag_id: req.params.tag_id,
+      },
+    }
+  ).then((updatedTag) => {
+    res.json(updatedTag);
+  }).catch ((err) => {
+    console.log(err);
+    res.json(err);
+  });
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if(!tagData) {
+      res.status(404).json({message: 'No tag found with this id!'});
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
